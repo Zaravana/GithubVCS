dac490a36782e18b1301847dbb6dcd037cfe8bf5
saravanakumars <shunmugavelsaravanakumar@gmail.com>
2018-02-08 11:10:55 +0530

    Second Commit

--- app/src/main/java/com/example/saravanakumars/githubvcs/MainActivity.java
+++ app/src/main/java/com/example/saravanakumars/githubvcs/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.saravanakumars.githubvcs;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"Hello World",Toast.LENGTH_LONG).show();
     }
 }
